c43723b403a6aa750dd7e74d75be865c5cebade7
Jussara6 <Jussara3856@gmail.com>
2026-01-12 09:48:22 -0300

    transformei em 2 funções e add for

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+  let tarefas =[]
+
 function adicionarTarefa() {
+
 
     let inputTarefa= document.getElementById("inputTarefa") // Pega o valor do input
     let tarefa= inputTarefa.value.trim() //salva o valor do input na variavel tarefa, .trim() remove espaços em branco no inicio e no fim da string
@@ -10,15 +14,14 @@ function adicionarTarefa() {
     return;
   }
     
-    let listaTarefas= document.getElementById("listaTarefas")//pega a ul onde as tarefas serão adicionadas
-    let novaTarefa = document.createElement("li") //cria um novo elemento li (lista não ordenada)
-    novaTarefa.textContent=tarefa //adiciona o valor da variavel tarefa no novo elemento li  
-    listaTarefas.appendChild(novaTarefa)
 
     mensagemEl.textContent= "Tarefa adicionada com sucesso" //adiciona o novo elemento li na ul
     mensagemEl.style.color = "green"; //muda a cor da mensagem para verde
     inputTarefa.value= "" 
     inputTarefa.focus() //foca o cursor no input após adicionar uma tarefa
+
+    tarefas.push(tarefa)
+    renderizarTarefas( )
 }
 document.getElementById("inputTarefa").addEventListener("keydown", function(e) {
   if (e.key === "Enter") {
@@ -28,3 +31,17 @@ document.getElementById("inputTarefa").addEventListener("keydown", function(e) {
   a Enter, chamara a função adicionarTarefa
   */
 });
+
+function renderizarTarefas(){
+   const listaTarefas= document.getElementById("listaTarefas")//pega a ul onde as tarefas serão adicionadas
+  listaTarefas.innerHTML=""
+
+  for (let i=0; i<tarefas.length;i++){
+
+    let novaTarefa = document.createElement("li") //cria um novo elemento li (lista não ordenada)
+    novaTarefa.textContent=tarefas[i] //adiciona o valor da variavel tarefa no novo elemento li  
+    listaTarefas.appendChild(novaTarefa)//faz a nova tarefa se tornar filha da lista de tarefas
+  }
+
+
+}
